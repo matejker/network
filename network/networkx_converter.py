ff0f0fadd0171f02474af345a820c60dd4d6151c
Matej Kerekrety <matej.kerekrety@gmail.com>
2021-01-03 12:16:37 +0000

    Add custome weight flag

--- network/networkx_converter.py
+++ network/networkx_converter.py
@@ -26,11 +26,12 @@ def network2networkx(network):
     return graph
 
 
-def networkx2network(graph):
+def networkx2network(graph, weight_key='weight'):
     """Converts networkx.Graph() into Network
 
     Args:
        graph (Graph): a Network object
+       weight_key (string): weight key in the graph (usually weight, length)
 
     Returns:
         A Network object.
@@ -41,11 +42,15 @@ def networkx2network(graph):
     n = len(nodes)
 
     directed = graph.is_directed()
-    weighted_edges = [(nodes.index(v), nodes.index(u), d['weight']) for (v, u, d) in graph.edges(data=True) if 'weight' in d]  # noqa
+    weighted_edges = [
+        (nodes.index(v), nodes.index(u), d[weight_key]) for (v, u, d) in graph.edges(data=True) if weight_key in d
+    ]
     weighted = len(weighted_edges) > 0
 
     if weighted:
-        unweighted_edges = [(nodes.index(v), nodes.index(u), 1) for (v, u, d) in graph.edges(data=True) if 'weight' not in d]  # noqa
+        unweighted_edges = [
+            (nodes.index(v), nodes.index(u), 1) for (v, u, d) in graph.edges(data=True) if weight_key not in d
+        ]
         edges = weighted_edges + unweighted_edges
     else:
         edges = [(nodes.index(v), nodes.index(u)) for v, u in graph.edges()]
